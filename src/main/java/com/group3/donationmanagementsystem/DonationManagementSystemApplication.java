b16dcb19803fdb8b6b38ff940f7f991c1d41bb3b
william-kl <hc740@scarletmail.rutgers.edu>
2021-05-12 00:34:27 -0400

    first commit

---
+++ src/main/java/com/group3/donationmanagementsystem/DonationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group3.donationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/donationmanagementsystem/DonationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.donationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
